ec1b32bcbfea7832d38bca86cea81fcf65a0e705
Jose Carlos Garcia Mendizabal <info@josegarciam.com>
2021-02-04 19:57:07 -0600

    new files

---
+++ graphql/schema/index.ts
@@ -0,0 +1,10 @@
+import {buildSchemaSync, Resolver, Query} from 'type-graphql';
+import {ImageResolver} from './image';
+import {HackafestResolver} from './hackafest';
+import {authChecker} from './auth';
+
+export const schema = buildSchemaSync({
+  resolvers: [ImageResolver, HackafestResolver],
+  emitSchemaFile: process.env.NODE_ENV === 'development',
+  authChecker,
+});
---
+++ pages/api/graphql.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+import {NextApiRequest} from 'next';
+import {ApolloServer} from 'apollo-server-micro';
+import {schema} from 'graphql/schema';
+import {Context} from 'graphql/schema/context';
+import {prisma} from 'prisma/client';
+import {loadIdToken} from 'firebase/admin';
+
+const server = new ApolloServer({
+  schema,
+  context: async ({req}: { req: NextApiRequest }): Promise<Context> => {
+    const uid = await loadIdToken(req);
+
+    return {
+      uid,
+      prisma,
+    };
+  },
+  tracing: process.env.NODE_ENV === 'development',
+});
+
+const handler = server.createHandler({path: '/api/graphql'});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
